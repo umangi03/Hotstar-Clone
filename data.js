d90f69f98ef44c8a7cc1b1a39fa4be3ffc9fe15a
Umangi Gore <umangigore@gmail.com>
2023-07-23 16:13:16 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,26 +2,26 @@ let movies=[
     {
         name: 'falcon and the winter soldier',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero rerum quam maiores ea quidem recusandae beatae aperiam facere earum quo.',
-        image: 'images/slider 2.png'
+        image: 'images/slider 2.PNG'
     },
     {
         name: 'loki',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero rerum quam maiores ea quidem recusandae beatae aperiam facere earum quo.',
-        image: 'images/slider 1.png'
+        image: 'images/slider 1.PNG'
     },
     {
         name: 'wanda vision',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero rerum quam maiores ea quidem recusandae beatae aperiam facere earum quo.',
-        image: 'images/slider 3.png'
+        image: 'images/slider 3.PNG'
     },
     {
         name: 'raya and the last dragon',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero rerum quam maiores ea quidem recusandae beatae aperiam facere earum quo.',
-        image: 'images/slider 4.png'
+        image: 'images/slider 4.PNG'
     },
     {
         name: 'luca',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero rerum quam maiores ea quidem recusandae beatae aperiam facere earum quo.',
-        image: 'images/slider 5.png'
+        image: 'images/slider 5.PNG'
     }
 ];
